44f41eec821687cc439a1001492fd176874aefc5
Eric Thornton <ericthornton43@gmail.com>
2022-08-31 13:23:10 -0600

    Added Col tags to ExpenseFormItem.

--- src/components/ExpenseFormItem.js
+++ src/components/ExpenseFormItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
@@ -21,10 +22,12 @@ class ExpenseFormItem extends Component {
     }
 
     return (
-      <Form.Group className="mb-3">
-        <Form.Label>{label}</Form.Label>
-        {contents}
-      </Form.Group>
+      <Col md={6}>
+        <Form.Group className="mb-3">
+          <Form.Label>{label}</Form.Label>
+          {contents}
+        </Form.Group>
+      </Col>
     );
   }
 }
