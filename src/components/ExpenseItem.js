948b9f619fb8ece3029e60b5d2b53e7411fb5c89
Eric Thornton <ericthornton43@gmail.com>
2022-09-02 19:05:05 -0600

    Add dynamic string truncation.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,7 @@ import React, { Component } from 'react';
 class ExpenseItem extends Component {
   render() {
     const { date, description, location, amount } = { ...this.props.expense };
-    let maxChars = 56;
-
-    const width = window.innerWidth;
-    if (width < 1200) maxChars = 32;
-    if (width < 992) maxChars = 24;
-    if (width < 1400) maxChars = 48;
-    if (width < 768) maxChars = 14;
+    const maxChars = this.props.maxChars;
 
     const dateString = new Date(date.replace('-', '/')).toLocaleDateString();
     const formatter = new Intl.NumberFormat('en-US', {
@@ -18,10 +12,10 @@ class ExpenseItem extends Component {
       minimumFractionDigits: 2
     });
     const amountString = formatter.format(amount);
-    const locationString = location.length > 32
+    const locationString = location.length > maxChars
       ? location.slice(0, maxChars) + '...'
       : location;
-    const descriptionString = description.length > 32
+    const descriptionString = description.length > maxChars
       ? description.slice(0, maxChars) + '...'
       : description;
 
