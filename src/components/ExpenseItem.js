9b95c3d2e62f31e235976ce63bc541eb0a332163
Eric Thornton <ericthornton43@gmail.com>
2022-09-03 10:34:20 -0600

    Change truncation to use CSS @media instead.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
+import '../styles/ExpenseItem.css';
 
 class ExpenseItem extends Component {
   render() {
     const { date, description, location, amount } = { ...this.props.expense };
-    const maxChars = this.props.maxChars;
 
     const dateString = new Date(date.replace('-', '/')).toLocaleDateString();
+
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2
     });
     const amountString = formatter.format(amount);
-    const locationString = location.length > maxChars
-      ? location.slice(0, maxChars) + '...'
-      : location;
-    const descriptionString = description.length > maxChars
-      ? description.slice(0, maxChars) + '...'
-      : description;
 
     return (
       <tr>
         <td className="text-center">{dateString}</td>
-        <td>{locationString}</td>
-        <td>{descriptionString}</td>
+        <td className="text-truncate truncate">{location}</td>
+        <td className="text-truncate truncate">{description}</td>
         <td className="text-danger text-end">{amountString}</td>
       </tr >
     );
--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -6,33 +6,15 @@ class ExpenseTable extends Component {
   constructor(props) {
     super(props);
     this.state = { width: window.innerWidth };
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-
-  }
-
-  handleResize() {
-    this.setState({ width: window.innerWidth });
   }
 
   render() {
     const expenses = this.props.expenses;
 
-    let maxChars = 56;
-
-    const width = this.state.width;
-    if (width < 1400) maxChars = 48;
-    if (width < 1200) maxChars = 32;
-    if (width < 992) maxChars = 24;
-    if (width < 768) maxChars = 14;
-
     const expensesToDisplay = [];
     for (let i = 0; i < expenses.length; i++) {
       expensesToDisplay.push(
-        <ExpenseItem key={i} expense={expenses[i]} maxChars={maxChars} />
+        <ExpenseItem key={i} expense={expenses[i]} />
       );
     }
 
