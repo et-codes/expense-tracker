38bfeab513b840dfa4214b26534c40b7c13a8b34
Eric Thornton <ericthornton43@gmail.com>
2022-09-06 20:16:32 -0600

    Change favicon.

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseTable from './components/ExpenseTable';
+import Footer from './components/Footer';
+
+const App = () => {
+  const getExpenses = () => {
+    let expensesJSON = localStorage.getItem('expenses');
+
+    // Delete legacy format data
+    if (JSON.parse(expensesJSON).expenses) {
+      localStorage.clear();
+      return { expenseList: [], total: 0 };
+    }
+
+    if (expensesJSON !== null) {
+      return JSON.parse(expensesJSON);
+    } else {
+      return { expenseList: [], total: 0 };
+    }
+  };
+
+  const [expenses, setExpenses] = useState(getExpenses());
+
+  const storeExpenses = () => {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  };
+
+  useEffect(storeExpenses, [expenses]);
+
+  const addExpense = (expense) => {
+    const updatedExpenses = expenses.expenseList.concat(expense);
+    const updatedTotal = expenses.total + expense.amount;
+    setExpenses({
+      expenseList: updatedExpenses,
+      total: updatedTotal
+    });
+  }
+
+  const removeExpense = (id) => {
+    const expenseToRemove = expenses.expenseList.find(exp => exp.id === id);
+    const newExpenses = expenses.expenseList.filter(exp => exp.id !== id);
+    const newTotal = expenses.total - expenseToRemove.amount;
+    setExpenses({
+      expenseList: newExpenses,
+      total: newTotal
+    });
+  }
+
+  return (
+    <Container className="p-3">
+      <Header total={expenses.total} />
+      <ExpenseForm handleSubmit={addExpense} />
+      <div className="mt-3">
+        <ExpenseTable
+          expenses={expenses.expenseList}
+          handleRemove={removeExpense}
+        />
+      </div>
+      <Footer />
+    </Container>
+  );
+}
+
+export default App;
